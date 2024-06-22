9a26720a88d32389e1c9f8ee6f1fd203627863f4
whyisn <wahyuisnan002@gmail.com>
2024-06-22 15:06:29 +0700

    Nav menu

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,7 +38,6 @@ android {
 }
 
 dependencies {
-
     implementation(platform("com.google.firebase:firebase-bom:33.1.1"))
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.firebase:firebase-auth:23.0.0")
